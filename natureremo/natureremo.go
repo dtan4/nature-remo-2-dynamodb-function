67b7e591a2edb19dd6f097245152496f1b04f9e1
Daisuke Fujita <dtanshi45@gmail.com>
2018-08-27 23:00:12 +0900

    Extract RoomMetrics to external package

--- natureremo/natureremo.go
+++ natureremo/natureremo.go
@@ -5,18 +5,13 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/tenntenn/natureremo"
+
+	"github.com/dtan4/nature-remo-2-dynamodb-function/types"
 )
 
 // Client wraps Nature Remo API Client
 type Client struct {
 	client *natureremo.Client
-}
-
-// RoomMetrics represents a set of room temperature, humidity and illuminance
-type RoomMetrics struct {
-	Temperature  float64
-	Humidity     float64
-	Illumination float64
 }
 
 // NewClient creates Nature Remo API Client wrapper
@@ -27,16 +22,16 @@ func NewClient(accessToken string) *Client {
 }
 
 // GetRoomMetrics returns available rooms temperature, humidity and illuminance
-func (c *Client) GetRoomMetrics(ctx context.Context) (map[string]*RoomMetrics, error) {
+func (c *Client) GetRoomMetrics(ctx context.Context) (map[string]*types.RoomMetrics, error) {
 	devices, err := c.client.DeviceService.Devices(ctx)
 	if err != nil {
-		return map[string]*RoomMetrics{}, errors.Wrap(err, "cannot get devices")
+		return map[string]*types.RoomMetrics{}, errors.Wrap(err, "cannot get devices")
 	}
 
-	metrics := map[string]*RoomMetrics{}
+	metrics := map[string]*types.RoomMetrics{}
 
 	for _, d := range devices {
-		metrics[d.ID] = &RoomMetrics{
+		metrics[d.ID] = &types.RoomMetrics{
 			Temperature:  d.NewestEvents[natureremo.SensorTypeTemperature].Value,
 			Humidity:     d.NewestEvents[natureremo.SensorTypeHumidity].Value,
 			Illumination: d.NewestEvents[natureremo.SensortypeIllumination].Value,
---
+++ types/types.go
@@ -0,0 +1,8 @@
+package types
+
+// RoomMetrics represents a set of room temperature, humidity and illuminance
+type RoomMetrics struct {
+	Temperature  float64
+	Humidity     float64
+	Illumination float64
+}
